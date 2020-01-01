03e6188fc183004e90eb86578b7f8cffbeadf1f7
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2020-01-01 14:23:29 +0100

    copy content to api folder

--- api/graphql.js
+++ api/graphql.js
@@ -1,20 +1,24 @@
-'use strict';
+'use strict'
 
-function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+function _interopDefault(ex) {
+  return ex && typeof ex === 'object' && 'default' in ex ? ex['default'] : ex
+}
 
-var apolloServerMicro = require('apollo-server-micro');
-var gql = _interopDefault(require('graphql-tag'));
-var dateFns = require('date-fns');
-var fs = require('fs');
-var path = require('path');
-var marked = _interopDefault(require('marked'));
-var highlightCode = _interopDefault(require('gatsby-remark-prismjs/highlight-code'));
-require('prismjs/components/prism-bash');
-require('prismjs/components/prism-typescript');
-require('prismjs/components/prism-json');
-require('prismjs/components/prism-textile');
-require('prismjs/components/prism-graphql');
-require('prismjs/components/prism-yaml');
+var apolloServerMicro = require('apollo-server-micro')
+var gql = _interopDefault(require('graphql-tag'))
+var dateFns = require('date-fns')
+var fs = require('fs')
+var path = require('path')
+var marked = _interopDefault(require('marked'))
+var highlightCode = _interopDefault(
+  require('gatsby-remark-prismjs/highlight-code'),
+)
+require('prismjs/components/prism-bash')
+require('prismjs/components/prism-typescript')
+require('prismjs/components/prism-json')
+require('prismjs/components/prism-textile')
+require('prismjs/components/prism-graphql')
+require('prismjs/components/prism-yaml')
 
 const typeDefs = gql`
   type Query {
@@ -55,7 +59,7 @@ const typeDefs = gql`
     date(displayAs: String): String
     tags: [String]
   }
-`;
+`
 
 const resolvers = {
   Query: {
@@ -93,7 +97,7 @@ const resolvers = {
   SnippetMetadata: {
     date,
   },
-};
+}
 
 function html(content, { htmlEntities }) {
   if (htmlEntities) {
@@ -123,29 +127,30 @@ const langs = {
   txt: 'textile',
   graphql: 'graphql',
   yml: 'yaml',
-};
+}
 
 function posts() {
-  const files = getFiles("../content/blog", '.md');
+  const files = getFiles('./content/blog', '.md')
   return files
     .map(file => {
       const { html, metadata, assetsSrc } = parseFileToHtmlAndMeta(file, {
         createAnchorAndFragment: (_level, metadata, text) => {
-          const anchorRegExp = /{([^}]+)}/g;
-          const anchorOverwrite = anchorRegExp.exec(text);
+          const anchorRegExp = /{([^}]+)}/g
+          const anchorOverwrite = anchorRegExp.exec(text)
           const fragment = anchorOverwrite
             ? anchorOverwrite[0].substring(2, anchorOverwrite[0].length - 1)
-            : slugify(text);
+            : slugify(text)
 
           return { anchor: `blog/${metadata.slug}#${fragment}`, fragment }
         },
-      });
+      })
 
-      const published = metadata.published === 'true';
-      const tags = metadata.tags.split(',').map(p => (p ? p.trim() : p));
-      const banner = path.join("https://timdeschryver.dev", assetsSrc, metadata.banner)
+      const published = metadata.published === 'true'
+      const tags = metadata.tags.split(',').map(p => (p ? p.trim() : p))
+      const banner = path
+        .join('https://timdeschryver.dev', assetsSrc, metadata.banner)
         .replace(/\\/g, '/')
-        .replace('/', '//');
+        .replace('/', '//')
 
       return {
         html,
@@ -162,7 +167,7 @@ function posts() {
 }
 
 function snippets() {
-  const files = getFiles("../content/snippets", '.md');
+  const files = getFiles('./content/snippets', '.md')
   return files
     .map(file => {
       const { html, metadata, assetsSrc } = parseFileToHtmlAndMeta(file, {
@@ -180,11 +185,12 @@ function snippets() {
               : '',
           ]
         },
-      });
-      const tags = metadata.tags.split(',').map(p => (p ? p.trim() : p));
-      const image = path.join("https://timdeschryver.dev", assetsSrc, metadata.image)
+      })
+      const tags = metadata.tags.split(',').map(p => (p ? p.trim() : p))
+      const image = path
+        .join('https://timdeschryver.dev', assetsSrc, metadata.image)
         .replace(/\\/g, '/')
-        .replace('/', '//');
+        .replace('/', '//')
 
       return {
         html,
@@ -203,32 +209,32 @@ function parseFileToHtmlAndMeta(
   file,
   { createAnchorAndFragment = () => {}, createHeadingParts = () => [] },
 ) {
-  const markdown = fs.readFileSync(file, 'utf-8');
-  const { content, metadata } = extractFrontmatter(markdown);
-  const assetsSrc = path.dirname(file.replace('content', ''));
-  const renderer = new marked.Renderer();
+  const markdown = fs.readFileSync(file, 'utf-8')
+  const { content, metadata } = extractFrontmatter(markdown)
+  const assetsSrc = path.dirname(file.replace('content', ''))
+  const renderer = new marked.Renderer()
 
   renderer.link = (href, title, text) => {
-    const href_attr = `href="${href}"`;
-    const title_attr = title ? `title="${title}"` : '';
-    const prefetch_attr = href.startsWith('/') ? `prefetch="true"` : '';
+    const href_attr = `href="${href}"`
+    const title_attr = title ? `title="${title}"` : ''
+    const prefetch_attr = href.startsWith('/') ? `prefetch="true"` : ''
     const attributes = [href_attr, title_attr, prefetch_attr]
       .filter(Boolean)
-      .join(' ');
+      .join(' ')
 
     return `<a ${attributes}>${text}</a>`
-  };
+  }
 
   renderer.image = function(href, _title, text) {
-    const src = href.startsWith('http') ? href : path.join(assetsSrc, href);
+    const src = href.startsWith('http') ? href : path.join(assetsSrc, href)
     return `<img src="${src}" alt="${text}" loading="lazy"/>`
-  };
+  }
 
   renderer.code = (source, lang) => {
-    lang = lang || 'txt';
+    lang = lang || 'txt'
 
-    const lineIndex = lang.indexOf('{');
-    const fileIndex = lang.indexOf(':');
+    const lineIndex = lang.indexOf('{')
+    const fileIndex = lang.indexOf(':')
 
     const language =
       lineIndex !== -1 || fileIndex !== -1
@@ -238,27 +244,27 @@ function parseFileToHtmlAndMeta(
               Math.min(...[lineIndex, fileIndex].filter(i => i !== -1)),
             )
             .trim()
-        : lang;
-    const prismLanguage = langs[language];
+        : lang
+    const prismLanguage = langs[language]
     const file =
-      fileIndex !== -1 ? lang.substr(lang.indexOf(':') + 1).trim() : '';
+      fileIndex !== -1 ? lang.substr(lang.indexOf(':') + 1).trim() : ''
 
-    const lineNumberRegExp = /{([^}]+)}/g;
-    const linesHighlight = [];
-    let curMatch;
+    const lineNumberRegExp = /{([^}]+)}/g
+    const linesHighlight = []
+    let curMatch
     while ((curMatch = lineNumberRegExp.exec(lang))) {
-      let parts = curMatch[1].split(',');
+      let parts = curMatch[1].split(',')
       parts.forEach(p => {
-        let [min, max] = p.split('-').map(Number);
-        max = max || min;
+        let [min, max] = p.split('-').map(Number)
+        max = max || min
         while (min <= max) {
-          linesHighlight.push(min++);
+          linesHighlight.push(min++)
         }
-      });
+      })
     }
 
     if (!prismLanguage) {
-      console.warn('did not found a language for: ' + language);
+      console.warn('did not found a language for: ' + language)
       return `<pre class='language-text'><code>${source}</code></pre>`
     }
 
@@ -266,30 +272,30 @@ function parseFileToHtmlAndMeta(
       prismLanguage,
       source,
       linesHighlight,
-    ).replace(/gatsby-highlight-code-line/g, 'line-highlight');
+    ).replace(/gatsby-highlight-code-line/g, 'line-highlight')
 
-    const codeBlock = `<code>${highlighted}</code>`;
-    const headingParts = [file, ...createHeadingParts(metadata)].filter(Boolean);
+    const codeBlock = `<code>${highlighted}</code>`
+    const headingParts = [file, ...createHeadingParts(metadata)].filter(Boolean)
     const heading = headingParts.length
       ? `<div class="code-heading">${headingParts.join(' ')}</div>`
-      : '';
+      : ''
     return `<pre class='language-${prismLanguage}'>${heading}${codeBlock}</pre>`
-  };
+  }
 
   renderer.codespan = source => {
     return `<code class="language-text">${source}</code>`
-  };
+  }
 
   renderer.heading = (text, level, rawtext) => {
     const headingText = text.includes('{')
       ? text.substring(0, text.indexOf('{') - 1)
-      : text;
+      : text
 
     const { anchor, fragment } = createAnchorAndFragment(
       level,
       metadata,
       rawtext,
-    );
+    )
     if (!anchor) {
       return `<h${level}>${headingText}</h${level}>`
     }
@@ -300,53 +306,53 @@ function parseFileToHtmlAndMeta(
           ${headingText}
         </a>
       </h${level}>`
-  };
+  }
 
   const html = marked(
     content.replace(/^\t+/gm, match => match.split('\t').join('  ')),
     { renderer },
-  );
+  )
 
   return { html, metadata, assetsSrc }
 }
 
 function getFiles(dir, extension, files = []) {
-  const dirFiles = fs.readdirSync(dir);
+  const dirFiles = fs.readdirSync(dir)
 
   dirFiles.forEach(path$1 => {
-    const file = path.join(dir, path$1);
-    const isDirectory = fs.lstatSync(file).isDirectory();
+    const file = path.join(dir, path$1)
+    const isDirectory = fs.lstatSync(file).isDirectory()
 
     if (isDirectory) {
       return getFiles(file, extension, files)
     }
 
     if (path.extname(file) === extension) {
-      files.push(file);
+      files.push(file)
     }
-  });
+  })
 
   return files
 }
 
 function extractFrontmatter(markdown) {
-  const match = /---\r?\n([\s\S]+?)\r?\n---/.exec(markdown);
-  const frontMatter = match[1];
-  const content = markdown.slice(match[0].length);
+  const match = /---\r?\n([\s\S]+?)\r?\n---/.exec(markdown)
+  const frontMatter = match[1]
+  const content = markdown.slice(match[0].length)
 
   const metadata = frontMatter.split('\n').reduce((data, pair) => {
-    const colonIndex = pair.indexOf(':');
-    data[pair.slice(0, colonIndex).trim()] = pair.slice(colonIndex + 1).trim();
+    const colonIndex = pair.indexOf(':')
+    data[pair.slice(0, colonIndex).trim()] = pair.slice(colonIndex + 1).trim()
     return data
-  }, {});
+  }, {})
 
   return { metadata, content }
 }
 
 function slugify(string) {
-  const a = 'àáäâãåăæçèéëêǵḧìíïîḿńǹñòóöôœøṕŕßśșțùúüûǘẃẍÿź·/_,:;';
-  const b = 'aaaaaaaaceeeeghiiiimnnnooooooprssstuuuuuwxyz------';
-  const p = new RegExp(a.split('').join('|'), 'g');
+  const a = 'àáäâãåăæçèéëêǵḧìíïîḿńǹñòóöôœøṕŕßśșțùúüûǘẃẍÿź·/_,:;'
+  const b = 'aaaaaaaaceeeeghiiiimnnnooooooprssstuuuuuwxyz------'
+  const p = new RegExp(a.split('').join('|'), 'g')
 
   return string
     .toString()
@@ -401,8 +407,8 @@ const apolloServerConfig = {
       },
     ],
   },
-};
+}
 
-const server = new apolloServerMicro.ApolloServer(apolloServerConfig);
+const server = new apolloServerMicro.ApolloServer(apolloServerConfig)
 
-module.exports = server.createHandler();
+module.exports = server.createHandler()
