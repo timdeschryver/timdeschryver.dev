4611f34e8fd239b34f25c2135387bfebfca8a8f3
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2020-07-11 13:58:52 +0200

    third time is a charm

--- src/routes/blog/latest.json.js
+++ src/routes/blog/latest.json.js
@@ -4,23 +4,19 @@ import { client } from '../../apollo-client'
 export async function get(req, res) {
   const response = await client(req).query({
     query: gql`
-      query($first: Int) {
-        posts(published: true, first: $first) {
+      query {
+        posts(published: true, first: 1) {
           metadata {
-            slug
             title
             description
-            date(displayAs: "human")
-            tags
+            canonical_url
+            banner
           }
         }
       }
     `,
-    variables: {
-      first: +req.query.first,
-    },
   })
 
   res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify(response.data, null, 2))
+  res.end(JSON.stringify(response.data.posts[0].metadata, null, 2))
 }
