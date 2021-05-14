85ccfc43516b5eea9553fcb38acabbefb66a127b
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2021-05-14 10:49:05 +0200

    add image to rss feed

--- src/routes/blog/rss.xml.ts
+++ src/routes/blog/rss.xml.ts
@@ -12,17 +12,23 @@ export function get() {
 
 function generate(allPosts: ReturnType<typeof posts>) {
 	const publishedPosts = allPosts.filter((post) => post.metadata.published);
-	const nodes = publishedPosts.map((post) => ({
-		title: post.metadata.title,
-		description: post.metadata.description,
-		link: `${import.meta.env.VITE_PUBLIC_BASE_PATH}/blog/${post.metadata.slug}`,
-		pubDate: UTCDate(post.metadata.date),
-		content: post.html
-			.replace(/&/g, '&amp;')
-			.replace(/</g, '&lt;')
-			.replace(/>/g, '&gt;')
-			.replace(/"/g, '&quot;')
-	}));
+	const nodes = publishedPosts.map((post) => {
+		const link = `${import.meta.env.VITE_PUBLIC_BASE_PATH}/blog/${post.metadata.slug}`;
+		return {
+			title: post.metadata.title,
+			description: post.metadata.description,
+			link,
+			pubDate: UTCDate(post.metadata.date),
+			content:
+				`<img class="webfeedsFeaturedVisual" src="${post.metadata.banner}" alt="${post.metadata.title}"/>` +
+				`<p>Read <strong>${post.metadata.title}</strong> on my <a href="${link}">blog</a></p>` +
+				post.html
+					.replace(/&/g, '&amp;')
+					.replace(/</g, '&lt;')
+					.replace(/>/g, '&gt;')
+					.replace(/"/g, '&quot;')
+		};
+	});
 
 	const itemNodes = nodes
 		.map((node) => {
@@ -50,9 +56,11 @@ function generate(allPosts: ReturnType<typeof posts>) {
 	${imageLink}
 	</image>`;
 	const xml = `<?xml version="1.0" encoding="UTF-8" ?>
-		<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
+		<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:webfeeds="http://webfeeds.org/rss/1.0" version="2.0">
 		<channel>
 		<title><![CDATA[ Tim Deschryver ]]></title>
+		<webfeeds:accentColor>F8C400</webfeeds:accentColor>
+		<webfeeds:analytics id="${import.meta.env.VITE_PUBLIC_GA_TRACKING_ID}" engine="GoogleAnalytics"/>
 		<description><![CDATA[ Blog by Tim Deschryver ]]></description>
 		${link}
 		${atom}
