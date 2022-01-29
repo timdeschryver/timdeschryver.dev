708f5ad5afc7ec907df6cc54cf37b4794eca2af2
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2022-01-29 15:07:18 +0100

    node

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import sveltePreprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
