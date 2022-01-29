b10e38b8c6aa77ab41b3809016187c6a0cadfdfa
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2022-01-29 16:26:09 +0100

    redirects

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,6 +4,9 @@ const config: PlaywrightTestConfig = {
 	forbidOnly: !!process.env.CI,
 	retries: process.env.CI ? 2 : 1,
 	timeout: 60_000,
+	outputDir: './test-results',
+	testDir: './tests',
+	reporter: 'html',
 	use: {
 		trace: 'on-first-retry',
 	},
