bcdcd9bcc6bce0ecf2a0e8c22b787e0401156b07
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2022-03-21 18:22:24 +0100

    update deps (#77)

--- svelte.config.js
+++ svelte.config.js
@@ -3,17 +3,13 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: sveltePreprocess(),
 	kit: {
+		trailingSlash: 'always',
 		prerender: {
 			entries: ['*', '/sitemap.xml', '/blog/rss.xml'],
+			default: true,
 		},
-
-		// By default, `npm run build` will create a standard Node app.
-		// You can create optimized builds for different platforms by
-		// specifying a different adapter
 		adapter: adapter(),
 	},
 };
