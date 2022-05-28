9ff223644bd93ee558b9bbd307bae333a89e507f
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2022-05-28 10:24:55 +0200

    move to SWA (#89)

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import sveltePreprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
