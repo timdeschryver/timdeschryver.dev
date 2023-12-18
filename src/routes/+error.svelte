155f4848448bde477128c6e823896e2ab13b9b50
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2023-12-18 15:38:19 +0100

    update to SvelteKit 2 (#301)

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,2 +1,5 @@
-<h1>Yikes. This is a bit awkward...</h1>
-<p>Go to the <a class="mark" href="/">Home</a> page, so we can quickly forget about this.</p>
+<h1>Yikes. This is a awkward...</h1>
+<p>
+	Go to the <a class="mark" href="/">Home</a> page so we can quickly forget about this, or search
+	for articals on the <a class="mark" href="/blog">Blog</a> page.
+</p>
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: '404.html',
+		}),
 	},
 };
