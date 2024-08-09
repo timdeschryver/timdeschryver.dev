4b6f5ea435418b192439ad45cdc7b1ffa7b4a036
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2024-08-09 17:53:30 +0200

    fix ad refresh

--- src/lib/Ad.svelte
+++ src/lib/Ad.svelte
@@ -1,8 +1,16 @@
-<div>
-	<script
-		async
-		type="text/javascript"
-		src="//cdn.carbonads.com/carbon.js?serve=CW7DV237&placement=timdeschryverdev&format=responsive"
-		id="_carbonads_js"
-	></script>
-</div>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	let ad: HTMLDivElement;
+
+	onMount(() => {
+		const script = document.createElement('script');
+		script.async = true;
+		script.type = 'text/javascript';
+		script.src =
+			'//cdn.carbonads.com/carbon.js?serve=CW7DV237&placement=timdeschryverdev&format=responsive';
+		script.id = '_carbonads_js';
+		ad.appendChild(script);
+	});
+</script>
+
+<div bind:this={ad}></div>
