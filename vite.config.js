4faa44ef4c98f31142683fdbcd2ab9a4260d2add
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2024-11-01 19:39:42 +0100

    fix tests

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,26 @@ import { defineConfig } from 'vitest/config';
 import VitePluginRestart from 'vite-plugin-restart';
 import svgLoader from 'vite-svg-loader';
 
-export default defineConfig({
-	plugins: [
-		sveltekit(),
-		svgLoader(),
-		VitePluginRestart({
-			restart: ['./blog/**', './bits/**', '!*.webp'],
-		}),
-	],
-	server: {
-		fs: {
-			allow: ['..'],
+export default defineConfig(({ mode }) => {
+	return {
+		plugins: [
+			sveltekit(),
+			svgLoader(),
+			VitePluginRestart({
+				restart: ['./blog/**', './bits/**', '!*.webp'],
+			}),
+		],
+		server: {
+			fs: {
+				allow: ['..'],
+			},
 		},
-	},
-	test: {
-		include: ['src/**/*.test.ts'],
-		environmentMatchGlobs: [['src/**/*.dom.test.ts', 'jsdom']],
-	},
+		test: {
+			include: ['src/**/*.test.ts'],
+			environmentMatchGlobs: [['src/**/*.dom.test.ts', 'jsdom']],
+		},
+		resolve: {
+			conditions: mode === 'test' ? ['browser'] : [],
+		},
+	};
 });
